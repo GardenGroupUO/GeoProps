6a63befbff41c1008c59622e1c73afffb4eea0cc
geoffreyweal <geoffrey.weal@gmail.com>
2021-05-16 13:02:10 +1200

    update version of GeoProps Program v1.2

--- GeoProps/__init__.py
+++ GeoProps/__init__.py
@@ -1,7 +1,7 @@
 # The information about the GeoProps program
 
 __name__    = 'The GeoProps Program'
-__version__ = '1.1'
+__version__ = '1.2'
 __author__  = 'Geoffrey Weal, Caitlin Casey-Stevens and Dr. Anna Garden'
 
 print('######################################################')
